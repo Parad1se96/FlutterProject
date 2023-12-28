0ecd93a9f67f5d08d20f4c2b7ad7500b3d5f19db
alexandreNdeO <nevesoliveira123@hotmail.com>
2023-12-28 22:04:21 +0000

    nada

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,15 +1,15 @@
 
 import 'package:flutter/material.dart';
 
-class SecondScreen extends StatefulWidget {//Para passar parametros é preferivel StatelessWidget passar para statefullwidget devido ao rendimento da app
-  const SecondScreen({super.key});
-  static const String routeName = '/SecondScreen';
+class ListScreen extends StatefulWidget {//Para passar parametros é preferivel StatelessWidget passar para statefullwidget devido ao rendimento da app
+  const ListScreen({super.key});
+  static const String routeName = '/ListScreen';
 
   @override
-  State<SecondScreen> createState() => _SecondScreenState();
+  State<ListScreen> createState() => _ListScreenState();
 }
 
-class _SecondScreenState extends State<SecondScreen> {
+class _ListScreenState extends State<ListScreen> {
 
   late int _counter = ModalRoute.of(context)?.settings.arguments as int ?? 0;//Desta forma recebemos os nosso argumentos
 
@@ -18,15 +18,15 @@ class _SecondScreenState extends State<SecondScreen> {
     return Scaffold(
       appBar: AppBar(
       title: const Hero(
-          tag: 'secondscreen',
-          child: Text('Second Screen')),
+          tag: 'listscreen',
+          child: Text('List Screen')),
         backgroundColor: Colors.amber,
       ),
       body:  Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Seconde Screen'),
+            Text('List Screen'),
             Text('>> $_counter <<'),
             ElevatedButton(onPressed: (){Navigator.pop(context, _counter * 2);}, child: const Text('Return')), //Leva para o MainScreen e com o pop dá para retornar o valor em dobro
           ],
